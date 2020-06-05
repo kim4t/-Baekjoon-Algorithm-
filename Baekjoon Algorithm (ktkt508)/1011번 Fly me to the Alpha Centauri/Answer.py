02865fc60a232f425e09a48fcd97e885730fb6cf
kim4t <taetaeho.kim@gmail.com>
2020-06-04 21:31:46 -0400

    no message

--- Baekjoon Algorithm (ktkt508)/1011번 Fly me to the Alpha Centauri/Answer.py
+++ Baekjoon Algorithm (ktkt508)/1011번 Fly me to the Alpha Centauri/Answer.py
@@ -1,2 +1,9 @@
-a,b = map(int, input().split())
-print(a-b)
+t = int(input())
+for i in range(t):
+   x,y = map(int,input().split())
+   distance = y-x
+   n = int((distance-1)**0.5)
+   if distance <= n*(n+1):
+       print(n*2)
+   else:
+       print(n*2+1)
